b4fd3aadc7f9eeb967166e2b9fd02a8c2f712483
KEVIN CHAWLA <kevin.chawla@mavs.uta.edu>
2020-08-05 00:37:49 -0500

    First Commit

---
+++ src/build/precache-manifest.e8e794958eb14f8d2f5bded6c164fdd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc5c6d3a84283137dd4c8d66997154a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4a6ef28c2b2491a8e2e",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "cc4f94076d1a05d8bfd3",
+    "url": "/static/js/2.bbc19163.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bbc19163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a6ef28c2b2491a8e2e",
+    "url": "/static/js/main.2e6422a3.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
